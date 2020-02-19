f0df603f46ca10de5ad742c29babaeff9afac858
BenMiriello <benmiriello@gmail.com>
2020-02-19 11:33:35 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+    end
+  end
+  
+end
---
+++ db/migrate/20200219153838_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.string :bio
+      t.string :avatar
+
+      t.timestamps
+    end
+  end
+end
