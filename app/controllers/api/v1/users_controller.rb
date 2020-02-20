7f6d0ba1980542cc5d70df663f48f597c8fe8aec
BenMiriello <benmiriello@gmail.com>
2020-02-20 11:17:45 -0500

    updated users controller to match account creation from front end

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,7 @@ class Api::V1::UsersController < ApplicationController
     end
    
     def create
+        # byebug
         @user = User.create(user_params)
         if @user.valid?
             @token = encode_token(user_id: @user.id)
