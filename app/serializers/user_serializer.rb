3754245b492e3616984705222bacd260a418ddc8
BenMiriello <benmiriello@gmail.com>
2020-02-23 17:50:40 -0500

    made serializer show user favorites and purchases

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :username, :avatar, :bio
+  attributes :username, :avatar, :bio, :favorites, :purchases
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_flight_offers, through: :favorites, source: :flight_offer
+
+  has_many :purchases, dependent: :destroy
+  has_many :purchased_flight_offers, through: :purchases, source: :flight_offer
+
 end
