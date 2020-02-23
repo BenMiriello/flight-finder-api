8eb5b556450884204e187e37770147cce02cd1c6
BenMiriello <benmiriello@gmail.com>
2020-02-23 18:39:50 -0500

    made user serializer serve up flight offer with all dependent associations, same as in search page

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,23 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :username, :avatar, :bio, :favorites, :purchases
+    attributes :username, :avatar, :bio, :favorited_flight_offers, :purchased_flight_offers
 
-  has_many :favorites, dependent: :destroy
-  has_many :favorited_flight_offers, through: :favorites, source: :flight_offer
+    def favorited_flight_offers
+        return self.object.favorited_flight_offers.map do |fo|
+            FlightOfferSerializer.new(fo).as_json
+        end
+    end
 
-  has_many :purchases, dependent: :destroy
-  has_many :purchased_flight_offers, through: :purchases, source: :flight_offer
+    def purchased_flight_offers
+        return self.object.purchased_flight_offers.map do |fo|
+            FlightOfferSerializer.new(fo).as_json
+        end
+    end
+
+    # has_many :favorites, dependent: :destroy
+    # has_many :favorited_flight_offers, through: :favorites, source: :flight_offer
+
+    # has_many :purchases, dependent: :destroy
+    # has_many :purchased_flight_offers, through: :purchases, source: :flight_offer
 
 end
+
