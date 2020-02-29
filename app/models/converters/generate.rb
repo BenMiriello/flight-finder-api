9ee2fd079db0800bad27b4a329ddefb8e9a0fa57
BenMiriello <benmiriello@gmail.com>
2020-02-29 10:05:40 -0500

    moved api request url generation to its own model

---
+++ app/models/converters/generate.rb
@@ -0,0 +1,34 @@
+class Generate
+
+    def self.url(searchParams, query)
+        url = "https://test.api.amadeus.com/v2/shopping/flight-offers"
+        origin = query[:originLocationCode].upcase
+        url += "?originLocationCode=#{origin}"
+        destination = query[:destinationLocationCode].upcase
+        url += "&destinationLocationCode=#{destination}"
+        dep_date = query[:departureDate][0...-14]
+        url += "&departureDate=#{dep_date}"
+        ret_date = query[:returnDate][0...-14]
+        url += "&returnDate=#{ret_date}"
+        url += "&adults=#{query[:adults]}"
+        url += "&nonStop=#{query[:nonStop]}"
+        
+        # Add valid optional parameters
+        if query[:children] > 0
+            url += "&children=#{query[:children]}"
+        end
+        
+        if query[:infants] > 0
+            url += "&infants=#{query[:infants]}"
+        end
+        
+        if query[:travelClass] == 'Economy' || 'Premium Economy' || 'Business' || 'First Class' 
+            class_name = query[:travelClass].parameterize.underscore.upcase
+            url += "&travelClass=#{class_name}"
+        end
+        
+        return url
+    end
+
+end
+
