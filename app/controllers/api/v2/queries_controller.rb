8db70c40c13857824a8bf6f56e3d2aba1d27f7e8
BenMiriello <benmiriello@gmail.com>
2020-08-18 22:55:52 -0400

    Build v2 queries create up to sending response to amadeus api

--- app/controllers/api/v2/queries_controller.rb
+++ app/controllers/api/v2/queries_controller.rb
@@ -21,5 +21,30 @@ class Api::V2::QueriesController < ApplicationController
         # Query object stores search info. Referenced when user makes the same search again.
         query_obj = Query.create(searchParams.permit(:originLocationCode, :destinationLocationCode, :departureDate, :returnDate, :travelClass, :adults, :children, :infants, :nonStop, :maxPrice, :origin_id, :destination_id, :user_id))
 
+        url = Url.generate(searchParams)
+        keys = Dotenv.load(".env")
+
+        # Amadeus API keys are needed here:
+        # Store in .env, format:
+        #   AMADEUS_CLIENT_ID=12345...
+        #   AMADEUS_CLIENT_SECRET=67890...
+        client = OAuth2::Client.new(
+            keys["AMADEUS_CLIENT_ID"],
+            keys["AMADEUS_CLIENT_SECRET"],
+            site: "https://test.api.amadeus.com",
+            token_url: "https://test.api.amadeus.com/v1/security/oauth2/token",
+        )
+
+        # New token is generated each request. Token is valid for 30 minutes.
+        token = client.client_credentials.get_token.token
+
+        def get_response(url, token)
+            RestClient::Request.execute(:method => :get, :url => url, :headers => { :Authorization => "Bearer #{token}" })
+        rescue => e
+            e
+        end
+
+        # Request is sent to Amadues API here:
+        raw_response = get_response(url, token)
     end
 end
